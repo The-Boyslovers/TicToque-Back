0fb8db6b0ba5aa880974265b5f204c4bd3d3527d
lorenzo <nizzolalorenzo@gmail.com>
2025-09-12 21:42:23 -0300

    First Commit

---
+++ TicToque/src/main/java/com/TBLS/TicToque/TicToqueApplication.java
@@ -0,0 +1,13 @@
+package com.TBLS.TicToque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicToqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicToqueApplication.class, args);
+	}
+
+}
---
+++ TicToque/src/test/java/com/TBLS/TicToque/TicToqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TBLS.TicToque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicToqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
